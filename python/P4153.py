32417022ce9a4038c64ba018533d9110f66a9f70
krepe90 <krepe90@gmail.com>
2021-06-29 05:21:46 +0900

    2021-06-29 (CLASS 2*)

---
+++ python/P4153.py
@@ -0,0 +1,9 @@
+# https://www.acmicpc.net/problem/4153
+while True:
+    a, b, c = sorted(map(int, input().split()))
+    if a == b == c == 0:
+        break
+    if (a**2 + b ** 2 == c ** 2):
+        print("right")
+    else:
+        print("wrong")
